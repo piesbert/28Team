675fc587f2fc1df171388462c0a5f0e4a0f732b4
Sebastian Szymak <sebastian.szymak@gmail.com>
2013-03-13 22:21:45 +0100

    Added GPL notice to source files.

--- head.php
+++ head.php
@@ -1,4 +1,23 @@
 <?php
+/* File: head.php
+ * Author: Sebasian Szymak
+ *
+ * This file is part of 28Team.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class Head extends Builder {
     private $element = null;
     private $keywords = '28team, geocaching, opencaching, geocoin, geocoins, travelbug, travelbugs, geokret, geokrety';
--- language.php
+++ language.php
@@ -1,4 +1,23 @@
 <?php
+/* File: language.php
+ * Author: Sebasian Szymak
+ *
+ * This file is part of 28Team.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if(isSet($_GET['lang'])) {
     $lang = $_GET['lang'];
 
